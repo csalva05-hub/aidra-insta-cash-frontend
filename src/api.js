881626edaef541bdef9d4677ae051fb30b26e149
csalva05-hub <carl.salva05@gmail.com>
2025-11-04 16:54:23 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,16 +1,9 @@
-import axios from 'axios';
+// src/api.js
+import axios from "axios";
+import CONFIG from "./config";
 
-const API_BASE = import.meta.env.VITE_API_URL || 'https://aidra-insta-cash-backend.onrender.com/api';
-
-const instance = axios.create({
-  baseURL: API_BASE,
-  headers: { 'Content-Type': 'application/json' }
+const api = axios.create({
+  baseURL: CONFIG.API_BASE_URL,
 });
 
-instance.interceptors.request.use(cfg => {
-  const token = localStorage.getItem('token');
-  if (token) cfg.headers.Authorization = 'Bearer ' + token;
-  return cfg;
-});
-
-export default instance;
+export default api;
